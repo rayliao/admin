aad05faaba8c51498385d6dd380b11b171b6f9bc
laoyl <liaoyl@mycode.best>
2017-02-24 18:29:17 +0800

    still confuse with typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Router, browserHistory  } from 'react-router'
+import { Provider } from 'react-redux'
+import { Map } from 'immutable'
 import './index.css';
 
 import configureRoutes from './routes'
+import configureStore from './utils/configureStore'
 
+const initialState = Map()
+const asyncReducers = {}
 const routes = configureRoutes()
-
+const store = configureStore(initialState, asyncReducers, {})
 
 ReactDOM.render(
-  <Router history={browserHistory}>
-    {routes}
-  </Router>,
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      {routes}
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
---
+++ src/utils/reducers.ts
@@ -0,0 +1,42 @@
+import { combineReducers } from 'redux-immutable'
+
+import routerReducer from './routerReducer'
+/**
+ * make root reducers
+ * @param {{}} asyncReducers
+ * @return combined reducers
+ */
+export const makeRootReducer = (asyncReducers: {}) => {
+  const reducers = { ...{ routing: routerReducer }, ...asyncReducers }
+  // hack:  fix Action type check.
+  return combineReducers(reducers) as any
+}
+
+/**
+ * dynamic inject a reducer to store 
+ * @param {any} store
+ * @param {{key:string,reducer:Function}} 
+ */
+export const injectReducer = (store, { key, reducer }, rootReducer?: Function) => {
+  /**
+   * add reducer to asyncReducers
+   */
+  store.asyncReducers[key] = reducer
+
+  /**
+   * make an app reducer
+   */
+  let appReducer = makeRootReducer(store.asyncReducers)
+  /**
+ * root reducer,handle logout
+ */
+  if (typeof rootReducer === 'function') {
+    appReducer = rootReducer(appReducer)
+  }
+  /**
+   * replace store's reducers with appReducer
+   */
+  store.replaceReducer(appReducer)
+}
+
+export default makeRootReducer
